a958b4eaf568f1f82cd0309f69d70302d994075c
Nick Peelman <nick@peelman.us>
2010-07-08 21:36:36 -0400

    Form looking better, more complete.  Added database field.

--- db/migrate/002_add_departments.rb
+++ db/migrate/002_add_departments.rb
@@ -2,6 +2,7 @@ class AddDepartments < ActiveRecord::Migration
   def self.up
     create_table :departments, :force => true do |t|
       t.string "name", :limit => 100, :null => false
+      t.string "abbreviation", :limit => 10, :null => true, :default => nil
       t.string "course_names", :limit => 200, :null => true, :default => nil
       t.string "contact_names", :limit => 200, :null => false, :default => nil
       t.string "contact_phones", :limit => 200, :null => true, :default => nil
