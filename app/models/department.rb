f229b45d1a076466de8fea812214467cb9df2f55
Nick Peelman <nick@peelman.us>
2010-07-09 16:00:06 -0400

    Ok, Now it works...

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,9 +2,8 @@ class Department < ActiveRecord::Base
   has_and_belongs_to_many :issues, :join_table => "issue_has_departments"
   has_many :contacts, :dependent => :destroy
 
-  validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_presence_of :name, :email
 
-  accepts_nested_attributes_for :contacts
+  accepts_nested_attributes_for :contacts, :reject_if => lambda { |contact| ( contact[:name].blank? || contact[:email].blank? ) }
   
 end
