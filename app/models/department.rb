989a59c75d30a233f6a2a1ad384afcec7ac3e23f
Nick Peelman <nick@peelman.us>
2010-07-09 15:06:22 -0400

    Putting Department Contacts in a table...

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,6 @@
 class Department < ActiveRecord::Base
   has_and_belongs_to_many :issues, :join_table => "issue_has_departments"
-  has_many :contacts
+  has_many :contacts, :dependent => :destroy
 
   validates_presence_of :name
   validates_uniqueness_of :name
